5f3a10010fbac269906efe9baf1017e719919adb
Zelis <zlhbstrk@hotmail>
2015-08-08 19:39:00 +0300

    1.gun yapilanlar

---
+++ gun1/ilk_dosyam.php
@@ -0,0 +1,21 @@
+<meta charset="utf-8" />
+<?php
+/*$a = (string) 22;
+
+// settype($a, "string");
+echo gettype($a);*/
+
+$sayi1=2;
+$sayi2=5;
+//echo $sayi1+$sayi2;
+//echo $sayi1-$sayi2;
+//echo $sayi1*$sayi2;
+//echo floor($sayi1/$sayi2);
+
+//echo $sayi1 % $sayi2;
+
+
+
+echo $$sayi2;
+
+?>
